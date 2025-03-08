c340a179bde88318c5ac4f035a968a73f1df2104
rajabpour4097 <rajabpour4097@gmail.com>
2025-03-08 23:14:03 +0330

    Change campaign images count upload

--- account/views.py
+++ account/views.py
@@ -9,7 +9,7 @@ from django.contrib.sites.shortcuts import get_current_site
 from django.utils.encoding import force_bytes, force_str
 from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
 from django.template.loader import render_to_string
-
+from jalali_date import datetime2jalali, date2jalali
 from account.models import CampaignTransaction, EditingCampaign, RequestForMentor
 from .tokens import account_activation_token
 from django.core.mail import EmailMessage
