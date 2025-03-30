2d614616c0639880362ddb5bb6756419358050e6
rajabpour4097 <rajabpour4097@gmail.com>
2025-03-30 20:02:21 +0330

    Change Portfolioslist view

--- account/templatetags/account_tags.py
+++ account/templatetags/account_tags.py
@@ -12,6 +12,13 @@ def jalali_timedate(value):
         return f"{jalali_date.year}-{jalali_date.month:02}-{jalali_date.day:02} {jalali_date.hour:02}:{jalali_date.minute:02}:{jalali_date.second:02}"
     return None
 
+@register.filter(name='jalali_date')
+def jalali_date(value):
+    if value:
+        jalali_date = jdatetime.datetime.fromgregorian(datetime=value)
+        return f"{jalali_date.year}-{jalali_date.month:02}-{jalali_date.day:02}"
+    return None
+
 @register.filter
 def add_class(value, arg):
     return value.as_widget(attrs={'class': arg})
