5ad11bcdaf4cd7944a634be30b590c2410f1ceb2
rajabpour4097 <rajabpour4097@gmail.com>
2025-04-28 18:17:35 +0330

    Solve admin panel problem

--- advplatform/admin.py
+++ advplatform/admin.py
@@ -51,7 +51,7 @@ class CustomUserAdmin(UserAdmin):
             'birth_date', 'field_of_activity',
             'user_type','cutomer_type','dealer_type',
             'rank','bussines_value',
-             'speciality_field', 'customer_mentor',
+             'speciality_field',
              'is_am'
             )}),
         ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
